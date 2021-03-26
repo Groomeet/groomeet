0414d10eccd65f568d9ee04099dda0aef2780af7
pedbiefre <pbiedmafresno@gmail.com>
2021-03-26 13:12:05 +0100

    Scrip empezado de creación de musicos musicos

---
+++ groomeet_backend/population/musicos.sql
@@ -0,0 +1,3 @@
+-- SQLite
+INSERT INTO auth_user (password, is_superuser, username, last_name, email, is_staff, is_active, date_joined, first_name)
+VALUES ("Probando0",0,"musico0","Perez","musico0@gmail.com",0,1,"2021-03-20 12:30:00.162018","Pablo");
