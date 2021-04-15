83b0ec7ca9df5f199665f2e285fe9c5d3e45f170
albparmor2 <albparmor2@alum.us.es>
2021-04-15 19:29:41 +0200

    Añadiendo a las bandas el atributo de imagen, enlace vídeo y descripción

--- groomeet_backend/models.py
+++ groomeet_backend/models.py
@@ -65,6 +65,10 @@ class Musico(models.Model):
     # def edad(self):
     #     return relativedelta(date.today(), self.fechaNacimiento).years
 
+#Método auxiliar para guardar la imagen como la id de la banda seguida de un punto
+def rename_image_banda(instance, filename):
+        filesplits = filename.split('.')
+        return 'media/images/bandas/%s.%s' % (instance.id, filesplits[-1])
 
 #Añadir ubicaciones para mejora del filtro de búsqueda
 class Banda(models.Model):
@@ -73,6 +77,9 @@ class Banda(models.Model):
     miembros = models.ManyToManyField(Musico, through='MiembroDe', blank=True)
     generos = models.ManyToManyField(Genero, blank=True)
     instrumentos = models.ManyToManyField(Instrumento, blank=True)
+    descripcion = models.TextField(verbose_name="Descripción")
+    enlaceVideo = models.URLField(verbose_name="Enlace de vídeo", blank=True)
+    imagen = models.ImageField(verbose_name="Imagen de la banda",upload_to=rename_image_banda, blank=True, null=True)
     #Sección de likes de Banda a Músico
     likesRecibidosMusico = models.ManyToManyField(User, related_name="likesDadosBanda", blank=True)
     noLikesRecibidosMusico = models.ManyToManyField(User, related_name="noLikesDadosBanda", blank=True)
