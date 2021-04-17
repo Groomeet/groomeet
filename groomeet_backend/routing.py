1449b1f2743c07e35e3febb1869bfab2187e0a81
enrmerver <eemv99@gmail.com>
2021-04-17 18:21:45 +0200

    Arreglado error chat

--- groomeet_backend/routing.py
+++ groomeet_backend/routing.py
@@ -4,10 +4,7 @@ from channels.auth import AuthMiddlewareStack
 from . import consumers
 from groomeet_backend import urls
 
-websocket_urlpatterns = [
-    re_path(r'^chat_ws$', consumers.ChatConsumer.as_asgi()),
-]
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
+    re_path(r'^ws/chat/(?P<room_name>[^/]+)/$', consumers.ChatConsumer.as_asgi()),
 ]
