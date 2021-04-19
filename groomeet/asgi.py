3cc35c9a29235d160509bde3b5f4136d13a1ce3b
enrmerver <eemv99@gmail.com>
2021-04-19 18:13:31 +0200

    fix

--- groomeet/asgi.py
+++ groomeet/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 import django
 import os
-
-from groomeet_backend import routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "groomeet.settings")
 django.setup()
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from groomeet_backend import routing
+
+
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
